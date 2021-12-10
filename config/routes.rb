69a703ec95b3c0bd1eca4bb802975c51236d30b2
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-12-10 13:50:43 -0600

    authentications controller & auth routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post '/auth/login', to: 'authentications#login'
+  get '/auth/verify', to: 'authentications#verify'
+  
   resources :posts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
