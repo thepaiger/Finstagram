2aa1f4cdf47d19aeda067dbd6474d2966f1269a2
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-12-13 12:00:21 -0600

    conditional rendering logout button working

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,38 +1,46 @@
 // Packages
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
 // Graphics
-import homeIcon from './../assets/graphics/home-icon.svg'
-import profileIcon from './../assets/graphics/profile-icon.svg'
-import logoutIcon from './../assets/graphics/logout-icon.svg'
+import homeIcon from "./../assets/graphics/home-icon.svg";
+import profileIcon from "./../assets/graphics/profile-icon.svg";
+import logoutIcon from "./../assets/graphics/logout-icon.svg";
 
 export default function Layout({ children, currentUser, handleLogout }) {
   const location = useLocation();
 
   return (
     <div>
-      {currentUser &&
-        <nav>
-
-        </nav>
-      }
       {children}
       <footer>
         {!currentUser ? (
           <div>
-            {(location.pathname === '/sign-in') ?
-              <p>Don't have an account? <Link to='/sign-up'>Sign Up.</Link></p>
-              : <p>Already have an account? <Link to='/sign-in'>Sign In.</Link></p>
-            }
+            {location.pathname === "/sign-in" ? (
+              <p>
+                Don't have an account? <Link to="/sign-up">Sign Up.</Link>
+              </p>
+            ) : (
+              <p>
+                Already have an account? <Link to="/sign-in">Sign In.</Link>
+              </p>
+            )}
           </div>
         ) : (
           <div>
-            <Link to='/'><img src={homeIcon} alt='home icon' /></Link>
-            <Link to={`/profile/${currentUser.id}`}><img src={profileIcon} alt='profile icon' /></Link>
-            <button onClick={handleLogout}><img src={logoutIcon} alt='logout icon' /></button>
+            <Link to="/">
+              <img src={homeIcon} alt="home icon" />
+            </Link>
+            <Link to={`/profile/${currentUser.id}`}>
+              <img src={profileIcon} alt="profile icon" />
+            </Link>
+            {location.pathname === `/profile/${currentUser.id}` ? (
+              <button onClick={handleLogout}>
+                <img src={logoutIcon} alt="logout icon" />
+              </button>
+            ) : null}
           </div>
         )}
-      </footer >
+      </footer>
     </div>
-  )
+  );
 }
