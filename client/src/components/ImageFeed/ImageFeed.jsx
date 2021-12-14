1b19e53c2a2e8db5e564f7f502863e500c5fa887
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-12-14 09:58:41 -0600

    CSS - MainFeed, ImageFeed mostly complete

--- client/src/components/ImageFeed/ImageFeed.jsx
+++ client/src/components/ImageFeed/ImageFeed.jsx
@@ -1,23 +1,32 @@
 // Packages
 import { Link } from 'react-router-dom';
 
+// CSS
+import './ImageFeed.css'
+
 export default function ImageFeed({ posts }) {
 
   return (
-    <div>
+    <div className="img-feed">
       {posts.map((post) => (
-        <article>
+        <article className="img-feed-post">
           {post.user?.username && (
             <>
-              <Link to={`/profile/${post.user.id}`}>
-                <img src={post.user.profile_pic_url} alt={`${post.user.username}'s Profile Picture`} />
-                <h4>{post.user.username}</h4>
+              <Link to={`/profile/${post.user.id}`} className="img-feed-heading">
+                <div className="img-feed-prof-pic-div">
+                  <img className="img-feed-prof-pic" src={post.user.profile_pic_url} alt={`${post.user.username}'s Profile Picture`} />
+                </div>
+                <h4 className="img-feed-username-top">{post.user.username}</h4>
               </Link>
-              <img src={post.img_url} alt={`${post.user.username}'s Post`} />
-              <Link to={`/profile/${post.user.id}`}>
-                <h6>{post.user.username}</h6>
-              </Link>
-              <p>{post.caption}</p>
+              <div className="img-feed-pic-div">
+                <img className="img-feed-pic" src={post.img_url} alt={`${post.user.username}'s Post`} />
+              </div>
+              <div className="img-feed-details">
+                <Link to={`/profile/${post.user.id}`}>
+                  <h6 className="img-feed-username-bottom">{post.user.username}</h6>
+                </Link>
+                <p className="img-feed-caption">{post.caption}</p>
+              </div>
             </>
           )}
         </article>
--- client/src/screens/MainFeed/MainFeed.jsx
+++ client/src/screens/MainFeed/MainFeed.jsx
@@ -7,13 +7,18 @@ import addIcon from './../../assets/graphics/add-icon.svg'
 // Components
 import ImageFeed from '../../components/ImageFeed/ImageFeed';
 
+// CSS
+import './MainFeed.css'
+
 export default function MainFeed({ posts }) {
   console.log(posts)
   return (
-    <div>
-      <header>
-        <h1>Finstagram</h1>
-        <Link to='/create-post'><img src={addIcon} alt='create post icon' /></Link>
+    <div className="main-feed">
+      <header className="main-feed-header">
+        <h1 className="main-feed-heading">Finstagram</h1>
+        <Link to='/create-post'>
+          <img src={addIcon} alt='create post icon' />
+        </Link>
       </header>
 
       <ImageFeed posts={posts} />
