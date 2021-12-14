7d6c5ad04341398839f88d6e34ce331055b8756b
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-12-14 13:06:16 -0600

    cleared warnings

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 // add conditional logic for production vs development environment
-const baseUrl = 'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://ph-finstagram.herokuapp.com/'
+    : 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: baseUrl,
