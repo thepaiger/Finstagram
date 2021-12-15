3ff72d360e0f98432daab05b31287f631480631c
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-12-14 18:15:18 -0600

    SelectedUser data implemented in SingleUserFeed/ImageGrid

--- client/src/screens/SingleUserFeed/SingleUserFeed.jsx
+++ client/src/screens/SingleUserFeed/SingleUserFeed.jsx
@@ -1,5 +1,6 @@
 // Packages
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 // Graphics
 import addIcon from './../../assets/graphics/add-icon.svg'
@@ -11,6 +12,24 @@ import ImageFeed from '../../components/ImageFeed/ImageFeed';
 import './SingleUserFeed.css'
 
 export default function SingleUserFeed({ posts }) {
+  const [userPosts, setUserPosts] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const { user_id } = useParams();
+
+  useEffect(() => {
+    const setPosts = () => {
+      setUserPosts(posts.filter((post) => post.user.id === Number(user_id)))
+    };
+    if (posts.length) setPosts();
+  }, [posts, user_id]);
+
+  useEffect(() => {
+    const setUser = () => {
+      setSelectedUser(userPosts[0]?.user)
+    };
+    if (userPosts) setUser();
+  }, [userPosts]);
+
   return (
     <div className="single-user-feed">
       <header className="single-user-feed-header">
@@ -20,7 +39,9 @@ export default function SingleUserFeed({ posts }) {
         </Link>
       </header>
 
-      <ImageFeed posts={posts} />
+      {selectedUser?.username && (
+        <ImageFeed posts={userPosts} />
+      )}
     </div>
   )
 }
